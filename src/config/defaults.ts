084daf3ecd4ee0bdc47db368f764cea64775df91
Adrian Obleton <obletonadrian@gmail.com>
2025-09-01 09:26:41 -0500

    Update defaults.ts to add more money in line with increasing costs

--- src/config/defaults.ts
+++ src/config/defaults.ts
@@ -5,7 +5,7 @@ export type Research = { Military: number; Grid: number; Nano: number };
 export type Resources = { credits: number; materials: number; science: number };
 
 export const INITIAL_RESEARCH: Research = { Military: 1, Grid: 1, Nano: 1 };
-export const INITIAL_RESOURCES: Resources = { credits: 10, materials: 5, science: 1 };
+export const INITIAL_RESOURCES: Resources = { credits: 20, materials: 5, science: 0 };
 
 // Base dock capacity
 export const INITIAL_CAPACITY = { cap: 3 } as const;
